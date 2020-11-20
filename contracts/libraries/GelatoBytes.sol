c9e07174ab6e98ed262194b85c64424cacccc358
gitpusha <gitpusha@gmail.com>
2020-11-20 10:29:27 +0100

    feat: extended GelatoBytes and GelatoString lib

--- contracts/libraries/GelatoBytes.sol
+++ contracts/libraries/GelatoBytes.sol
@@ -1,5 +1,5 @@
 // "SPDX-License-Identifier: UNLICENSED"
-pragma solidity ^0.6.10;
+pragma solidity >=0.6.10;
 
 library GelatoBytes {
     function calldataSliceSelector(bytes calldata _bytes)
@@ -26,22 +26,58 @@ library GelatoBytes {
             (bytes4(_bytes[3]) >> 24);
     }
 
-    function revertWithErrorString(bytes memory _bytes, string memory _tracingInfo)
+    function revertWithError(bytes memory _bytes, string memory _tracingInfo)
         internal
         pure
     {
         // 68: 32-location, 32-length, 4-ErrorSelector, UTF-8 err
         if (_bytes.length % 32 == 4) {
             bytes4 selector;
-            assembly { selector := mload(add(0x20, _bytes)) }
-            if (selector == 0x08c379a0) {  // Function selector for Error(string)
-                assembly { _bytes := add(_bytes, 68) }
+            assembly {
+                selector := mload(add(0x20, _bytes))
+            }
+            if (selector == 0x08c379a0) {
+                // Function selector for Error(string)
+                assembly {
+                    _bytes := add(_bytes, 68)
+                }
                 revert(string(abi.encodePacked(_tracingInfo, string(_bytes))));
             } else {
-                revert(string(abi.encodePacked(_tracingInfo, "NoErrorSelector")));
+                revert(
+                    string(abi.encodePacked(_tracingInfo, "NoErrorSelector"))
+                );
             }
         } else {
-            revert(string(abi.encodePacked(_tracingInfo, "UnexpectedReturndata")));
+            revert(
+                string(abi.encodePacked(_tracingInfo, "UnexpectedReturndata"))
+            );
+        }
+    }
+
+    function returnError(bytes memory _bytes, string memory _tracingInfo)
+        internal
+        pure
+        returns (string memory)
+    {
+        // 68: 32-location, 32-length, 4-ErrorSelector, UTF-8 err
+        if (_bytes.length % 32 == 4) {
+            bytes4 selector;
+            assembly {
+                selector := mload(add(0x20, _bytes))
+            }
+            if (selector == 0x08c379a0) {
+                // Function selector for Error(string)
+                assembly {
+                    _bytes := add(_bytes, 68)
+                }
+                return string(abi.encodePacked(_tracingInfo, string(_bytes)));
+            } else {
+                return
+                    string(abi.encodePacked(_tracingInfo, "NoErrorSelector"));
+            }
+        } else {
+            return
+                string(abi.encodePacked(_tracingInfo, "UnexpectedReturndata"));
         }
     }
 }
